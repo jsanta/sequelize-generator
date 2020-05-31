17d6f0873e1826a00abe522f4b4d97e7bb1dd353
Jose Ignacio Santa Cruz G <j@santa.cl>
2020-05-31 09:24:03 -0400

    CHG Type checking and object initalization are handled as promises. That was the only way to achieve a "clean" on demand schema-loading to avoid  slow startup
    Signed-off-by: Jose Ignacio Santa Cruz G <j@santa.cl>

--- src/entitytype-generator/files/T__name@classify__.ts
+++ src/entitytype-generator/files/T__name@classify__.ts
@@ -1,6 +1,6 @@
 import { I<%= classify(name) %> } from 'interface/module'; // CHANGE ME!
 // Copy files from node_modules/sequelize-generator-schematic/src/utils 
-import { autoImplement } from '../utils'; // Requires extra utils module for additional validations
+import { autoImplement } from '../utils';
 
 // Declares a type so all interface properties are optional
 // Ref https://stackoverflow.com/questions/54986332/typescript-class-extending-partial-interface
@@ -15,18 +15,29 @@ export class T<%= classify(name) %> extends autoImplement<Optional<%= classify(n
   /**
    * Checks object against the json-schema generated for the parent interface
    * implemented by this type class.
-   * @param value Value to be checked 
-   * @returns true if validation succeeds (object can be safely casted to the type being validated), 
+   * @param value Value to be checked
+   * @returns true if validation succeeds (object can be safely casted to the type being validated),
    *          false otherwise (should be handled as an Error)
    */
-  static check(value: any): boolean {
-    if (!this.entityValidator) {
-      // entityValidator has to be instanced before validating an object
-      throw Error('No se puede realizar la validación si no se ha instanciado entityValidator');
+  static check(value: any): Promise<boolean> {
+    try {
+      let result: Promise<boolean>;
+      if (!this.entityValidator || !this.entityValidator.schemaCache[schemaKey]) {
+        // entityValidator has to be instanced before validating an object
+        // Beware: This operation IS SLOW!
+        result = _initT<%= classify(name) %>().then(
+          ev => { return this.check(value); }
+        );
+      } else {
+        result = Promise.resolve(this.entityValidator.check(value, schemaKey));
+      }
+      return result;
+    } catch (evErr) {
+      console.error('No se puede realizar la validación usando schema para ', schemaKey, evErr);
+      throw Error(`No se puede realizar la validación usando schema para ${schemaKey}`);
     }
-    return this.entityValidator.check(value, schemaKey);
   }
-  
+
   /*
   // Constructor if required
   public constructor() {
@@ -40,22 +51,42 @@ export class T<%= classify(name) %> extends autoImplement<Optional<%= classify(n
 // schemaKey used to retrieve the json validation schema
 const schemaKey: string = 'T<%= classify(name) %>';
 
-// Schema loading is placed on a external try so this expensive task takes place when the app is starting.
-// Can fail if the EntityValidator is not available.
-try {
-  // Uses a dynamic import so the type class is not dependant on the entityValidator
-  import('../utils').then(
-    utils => {
-      T<%= classify(name) %>.entityValidator = utils.entityValidator;
+async function _initT<%= classify(name) %>() {
+  // Schema loading is placed on a external function so this expensive task takes place on demand.
+  // Can fail if the EntityValidator is not available.
+  try {
+    // Uses a dynamic import so the type class is not dependant on the entityValidator
+    return import('../utils').then(
+      utils => {
+        T<%= classify(name) %>.entityValidator = utils.entityValidator;
 
-      const filePath: string  = './path/to/<%= name %>.model.ts'; // CHANGE ME!
-      const typeName: string  = 'I<%= classify(name) %>';
-      (utils.entityValidator).loadSchema(schemaKey, filePath, typeName);
-    },
-    // Could not load utils library
-    err => console.error('No se pudo cargar a libreria utils')
-  );
-} catch (evErr) {
-  // Could not load json-schema for schemaKey
-  console.error('No fue posible cargar el json-schema para: ', schemaKey, evErr);
+        const filePath: string = './libs/qpmodel/src/lib/model/global/<%= name %>.model.ts';
+        const typeName: string = 'I<%= classify(name) %>';
+        // Async schema loading
+        // Slow task
+        // const schemaPromise: Promise<{ schemaKey: string, schema: any }> = (utils.entityValidator).asyncLoadSchema(schemaKey, filePath, typeName);
+        // (utils.typeClassSchemas).push(schemaPromise);
+        // schemaPromise.then(
+        //   sp => {
+        //     utils.entityValidator.schemaCache[sp.schemaKey] = sp.schema;
+        //   },
+        //   spErr => console.error(spErr)
+        // );
+
+        // Sync schema loading
+        // Slow task
+        const schema: any = (utils.entityValidator).syncLoadSchema(schemaKey, filePath, typeName);
+        utils.entityValidator.schemaCache[schemaKey] = schema;
+
+        return T<%= classify(name) %>.entityValidator;
+      },
+      // Could not load utils library
+      err => console.error('No se pudo cargar a libreria utils')
+    );
+  } catch (evErr) {
+    // Could not load json-schema for schemaKey
+    console.error('No fue posible cargar el json-schema para: ', schemaKey, evErr);
+  }
 }
+// Not so good idea, because it slows down the application start
+// _initT<%= classify(name) %>();
